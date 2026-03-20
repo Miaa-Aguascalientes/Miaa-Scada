ce7bda12c5fa82842f23190d079c83c4b85ebdcf
Miaa <pedroguillermomiaa@gmail.com>
2026-03-20 11:19:37 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -370,7 +370,7 @@ for id_p, info in mapa_pozos_dict.items():
 # Dibujamos el marcador con tu RADIO 3
     folium.CircleMarker(
         location=info['coord'],
-        radius=3, 
+        radius=5, 
         color=info['color_final'],
         fill=True,
         fill_color=info['color_final'],

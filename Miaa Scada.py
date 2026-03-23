bd7ba17c0c70c369137ef208e2fb542b566370b5
Miaa <pedroguillermomiaa@gmail.com>
2026-03-23 10:45:09 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -301,6 +301,7 @@ with st.sidebar:
                 st.write(f"⚪ {p}")
 
 # 7--------------------------------------------------------------------------------- SECCION 7. MAPA -------------------------------------------------------------------------------------------------------------
+col_mapa, col_capas = st.columns([8.5, 1.5])
 with col_capas:
     st.markdown("### 🗺️ Capas")
     ver_sectores = st.checkbox("Sectores", value=True)

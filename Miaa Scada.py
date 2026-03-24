0317ef0201bdc2a3c5f7c1d5ac9bb662638c45eb
Miaa <pedroguillermomiaa@gmail.com>
2026-03-24 14:47:40 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -513,6 +513,10 @@ with st.sidebar:
         st.markdown(f"**BD-Diccionarios:** {get_tag(status_mysql_tele)}", unsafe_allow_html=True)
         st.markdown(f"**BD-PostgreSQL:** {get_tag(status_postgres)}", unsafe_allow_html=True)
 
+    # --- BUSCADOR DE POZOS ---
+    lista_pozos = sorted(list(mapa_pozos_dict.keys()))
+    pozo_buscado = st.selectbox("🔍 Buscar Pozo:", ["Seleccionar..."] + lista_pozos, index=0)
+
     if st.button("♻️ Actualizar Datos", use_container_width=True):
         st.cache_data.clear()
         st.cache_resource.clear()
@@ -562,6 +566,10 @@ col_mapa, col_capas = st.columns([0.9, 0.1], gap="small")
 with col_mapa:
     m = folium.Map(location=[21.8820, -102.2800], zoom_start=12.5, tiles="CartoDB dark_matter")
     Fullscreen().add_to(m)
+
+    if pozo_buscado != "Seleccionar..." and pozo_buscado in mapa_pozos_dict:
+    lat_centro, lon_centro = mapa_pozos_dict[pozo_buscado]['coord']
+    zoom_inicial = 16 # Zoom cercano al pozo encontrado
 
     # FUNCIÓN PARA HORARIO 00:00
     def formato_hora(decimal):

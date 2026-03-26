ff12ed5b52350c33898405392ab7cf61de342e63
Miaa <pedroguillermomiaa@gmail.com>
2026-03-26 15:43:51 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -906,9 +906,14 @@ with col_mapa:
                 tooltip=f"Tanque: {info['nombre']}"
 
                 
-                folium.Marker(location=info['coord'],icon=folium.DivIcon(icon_anchor=(20, -10),html=f'<div style="font-size: 9px; font-weight: bold; color: #00d4ff; text-shadow: 1px 1px #000;">{id_tq}</div>'
-
-                
+# Etiqueta de texto ID fija
+                folium.Marker(
+                    location=info['coord'],
+                    icon=folium.DivIcon(
+                        icon_anchor=(20, -10),
+                        html=f'<div style="font-size: 9px; font-weight: bold; color: #00d4ff; text-shadow: 1px 1px #000;">{id_tq}</div>'
+                    )
+                ).add_to(m)
             except:
                 continue
             

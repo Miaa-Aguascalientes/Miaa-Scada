7e9f249192aab52122e70fa0ddbe8624d9301d27
Miaa <pedroguillermomiaa@gmail.com>
2026-03-30 19:27:44 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -8,6 +8,7 @@ import psycopg2
 import json
 import urllib.parse
 from datetime import datetime
+from streamlit_autorefresh import st_autorefresh
 
 # 1  SECCION---------------------------------------------------------------------------1. CONFIGURACIÓN DE PÁGINA ----------------------------------------------------------------------------------------------------------
 params = st.query_params
@@ -24,6 +25,7 @@ st.set_page_config(
     layout="wide", 
     initial_sidebar_state="expanded"
 )
+count = st_autorefresh(interval=30000, limit=1000, key="scada_refresh")
 
 # 2  SECCION------------------------------------------------------------------------------2. FUNCIONES DE CONEXIÓN ------------------------------------------------------------------------------------------------------
 @st.cache_resource
